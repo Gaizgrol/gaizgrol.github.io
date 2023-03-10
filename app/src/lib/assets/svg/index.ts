500447f875c8e785b1fdb55c1d581347b030d798
Gaizgrol <gabrielizotongo@gmail.com>
2023-03-10 07:25:02 -0300

    feat: arquivos de ícone

---
+++ app/src/lib/assets/svg/index.ts
@@ -0,0 +1,77 @@
+import agileSvg from '$lib/assets/svg/agile.svg'
+import angularSvg from '$lib/assets/svg/angular.svg'
+import bulbSvg from '$lib/assets/svg/bulb.svg'
+import cSvg from '$lib/assets/svg/c.svg'
+import classroomSvg from '$lib/assets/svg/classroom.svg'
+import comunicacaoSvg from '$lib/assets/svg/comunicacao.svg'
+import conflitosSvg from '$lib/assets/svg/conflitos.svg'
+import cppSvg from '$lib/assets/svg/cpp.svg'
+import css3Svg from '$lib/assets/svg/css3.svg'
+import djangoSvg from '$lib/assets/svg/django.svg'
+import dockerSvg from '$lib/assets/svg/docker.svg'
+import equipeSvg from '$lib/assets/svg/equipe.svg'
+import expressSvg from '$lib/assets/svg/express.svg'
+import gitSvg from '$lib/assets/svg/github.svg'
+import githubSvg from '$lib/assets/svg/github.svg'
+import html5Svg from '$lib/assets/svg/html5.svg'
+import ionicSvg from '$lib/assets/svg/ionic.svg'
+import javaSvg from '$lib/assets/svg/java.svg'
+import jestSvg from '$lib/assets/svg/jest.svg'
+import jsSvg from '$lib/assets/svg/js.svg'
+import laravelSvg from '$lib/assets/svg/laravel.svg'
+import liderancaSvg from '$lib/assets/svg/lideranca.svg'
+import mongodbSvg from '$lib/assets/svg/mongodb.svg'
+import mysqlSvg from '$lib/assets/svg/mysql.svg'
+import nodejsSvg from '$lib/assets/svg/nodejs.svg'
+import oratoriaSvg from '$lib/assets/svg/oratoria.svg'
+import phpSvg from '$lib/assets/svg/php.svg'
+import pythonSvg from '$lib/assets/svg/python.svg'
+import railsSvg from '$lib/assets/svg/rails.svg'
+import reactSvg from '$lib/assets/svg/react.svg'
+import rubySvg from '$lib/assets/svg/ruby.svg'
+import scrapingSvg from '$lib/assets/svg/scraping.svg'
+import serverSvg from '$lib/assets/svg/server.svg'
+import sheetsSvg from '$lib/assets/svg/sheets.svg'
+import slidesSvg from '$lib/assets/svg/slides.svg'
+import testsSvg from '$lib/assets/svg/tests.svg'
+import tsSvg from '$lib/assets/svg/ts.svg'
+
+export {
+    agileSvg,
+    angularSvg,
+    bulbSvg,
+    cSvg,
+    classroomSvg,
+    comunicacaoSvg,
+    conflitosSvg,
+    cppSvg,
+    css3Svg,
+    djangoSvg,
+    dockerSvg,
+    equipeSvg,
+    expressSvg,
+    gitSvg,
+    githubSvg,
+    html5Svg,
+    ionicSvg,
+    javaSvg,
+    jestSvg,
+    jsSvg,
+    laravelSvg,
+    liderancaSvg,
+    mongodbSvg,
+    mysqlSvg,
+    nodejsSvg,
+    oratoriaSvg,
+    phpSvg,
+    pythonSvg,
+    railsSvg,
+    reactSvg,
+    rubySvg,
+    scrapingSvg,
+    serverSvg,
+    sheetsSvg,
+    slidesSvg,
+    testsSvg,
+    tsSvg
+}
